7b66ac9312e74c4aa0468c2b6020c3532589b7fc
hoseong511 <thdghtjd115@naver.com>
2021-03-29 04:18:16 +0000

    update or and elif

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-# if else; is 
-def plus(a, b):
-  if type(a) is str:
-    return None
-  else:
-    return a+b
+# or and, elif
+def age_check(age):
+  print(f"you are {age}")
+  if age < 18:
+    print("you cant drink")
 
-result = plus("3", "3")
-print(result)
+  elif age == 18 or age == 19:
+    print("you are new to this!")
+  elif age > 20 and age < 25:
+    print("you are still kind of young")
+  else:
+    print("enjoy your drink")
+
+age_check(19)
