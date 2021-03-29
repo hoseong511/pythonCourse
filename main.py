6a6de662645404cd428265e2da9b2fd4d00c4e9e
hoseong511 <thdghtjd115@naver.com>
2021-03-29 03:03:17 +0000

    update tuple, dictionary

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
+# mutable sequence
+
 days = ["mon", "tue", "wed", "thur", "fri"]
 
 print("mon" in days)
+
+# immutable sequence : tuple
+
+days = ("mon", "tue", "wed", "thur", "fri")
+
+# dictionary, object Mgmt
+hoho = {
+  "name" : "ho",
+  "age" : "20",
+  "fav_food" : ["kimchi","ham"],
+  "car" : False
+}
+hoho["fav_food"].append("chicken")
+hoho["age"] = 25
+print(hoho)
