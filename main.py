a5a6fb1284616907d9025d584f3b7450bd136175
hoseong511 <thdghtjd115@naver.com>
2021-03-29 04:03:48 +0000

    update if else

--- main.py
+++ main.py
@@ -1,36 +1,6 @@
-def plus(x, y):
-    x, y = map(int, [x, y])
-    print(x + y)
-
-
-def diff(x, y):
-    x, y = map(int, [x, y])
-    print(x - y)
-
-
-def mul(x, y):
-    x, y = map(int, [x, y])
-    print(x * y)
-
-
-def div(x, y):
-    x, y = map(int, [x, y])
-    print(x / y)
-
-
-def floor(x, y):
-    x, y = map(int, [x, y])
-    print(x // y)
-
-
-def remainder(x, y):
-    x, y = map(int, [x, y])
-    print(x % y)
-
-
-def power(x, y):
-    x, y = map(int, [x, y])
-    print(x**y)
-
-
-mul(2, "5")
+# if else; is 
+def plus(a, b):
+  if type(b) is str:
+    return None
+  else:
+    return a+b
