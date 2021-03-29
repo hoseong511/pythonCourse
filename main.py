8eb3a563f84b474624ae12f3dbd1437a09d4ae6d
hoseong511 <thdghtjd115@naver.com>
2021-03-29 03:26:28 +0000

    update keyword argument

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-# return
-def p_plus(a, b):
-    print(a + b)
+# keyword Argument; 따로 지정하지 않는다면 순서를 지켜 인자에 넣어주어야한다.
+def say_hello(name, age):
+  return f"Hello {name} you are {age} years old"
 
-def r_plus(a, b):
-    return a + b
-
-
-p_result = p_plus(2, 3)
-r_result = r_plus(2, 3)
-
-print(p_result, r_result)
+hello = say_hello(age="12", name="hoho")
+print(hello)
